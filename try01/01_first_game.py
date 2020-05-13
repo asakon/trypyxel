04621294c544e10e0d29b0095cb2be08466e0d33
asakon <kondoasako@gmail.com>
2020-05-13 13:08:18 +0900

    コメント

--- try01/01_first_game.py
+++ try01/01_first_game.py
@@ -2,10 +2,12 @@ import pyxel
 
 pyxel.init(160, 120)
 
+# フレーム処理
 def update():
     if pyxel.btnp(pyxel.KEY_Q):
         pyxel.quit()
 
+# 描画処理
 def draw():
     pyxel.cls(0)
     pyxel.rect(10, 10, 20, 20, 11)
