299bc05bd5b42f02a81b3f87191bdf524690129d
asakon <kondoasako@gmail.com>
2020-05-14 05:05:41 +0900

    init

---
+++ try01/02_try01.py
@@ -0,0 +1,19 @@
+import pyxel
+
+class App:
+	def __init__(self):
+		# アプリの画面サイズ。幅、高さ、キャプション
+		pyxel.init(160, 120, caption="Catio")
+
+		# アプリの開始。 
+		pyxel.run(self.update, self.draw)
+
+	def update(self):
+		pyxel.show()
+
+	def draw(self):
+		# 画面を色でクリアする
+		pyxel.cls(0)
+
+
+App()
